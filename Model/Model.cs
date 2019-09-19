2b0d8fe63166863cb0d35537caccd4c1c4fb5279
David Stripeik <dpstrip@outlook.com>
2019-09-18 19:09:28 -0500

    Add project files.

---
+++ Model/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Model
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Make Make { get; set;} //This is a navigation property so I can call objects in reverse direction
+
+        public int MakeId { get; set; } //This is to tell the system to worry about the foreign key
+    }
+}
---
+++ Persistance/VegaDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Model;
+
+namespace Vega.Persistance
+{
+    public class VegaDbContext: DbContext
+    {
+        //class is creating the database content
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Make> Makes { get; set; }
+
+    }
+}
